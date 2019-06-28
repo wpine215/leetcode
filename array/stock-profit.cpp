6685e558756ef71793cd2d51083755c101d44c80
Will Pine <wpine215@gmail.com>
2019-06-28 13:34:07 -0700

    Create stock-profit.cpp

---
+++ array/stock-profit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+    }
+};
+
+// TESTING
+int main() {
+    // Expected output: 5
+	vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+	Solution s;
+	int output = s.twoSum(input, tar);
+
+	cout << output << endl;
+	return 0;
+}
