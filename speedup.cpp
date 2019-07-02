3974053fa3642e91b6b0578ac0d6d3b36bb0915a
wpine215 <wpine215@gmail.com>
2019-07-02 19:26:12 -0400

    Added optimization code snippet

---
+++ speedup.cpp
@@ -0,0 +1,7 @@
+// Include this at the top of every Leetcode submission
+static int speed = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
