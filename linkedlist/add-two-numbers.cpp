a3b960dfa58a164755f0804169707d0326353d6e
wpine215 <wpine215@gmail.com>
2019-08-16 21:09:47 -0400

    Completed problem, still not passing all test cases

--- linkedlist/add-two-numbers.cpp
+++ linkedlist/add-two-numbers.cpp
@@ -13,6 +13,38 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        int sum;
+        int carry = (l1->val + l2->val) / 10;
+        ListNode* head = new ListNode((l1->val + l2->val) % 10);
+        ListNode* curr = head;
+        l1 = l1->next;
+        l2 = l2->next;
+
+        while (l1 != NULL) {
+            if (l2 != NULL) {
+                sum = (l1->val + l2->val + carry) % 10;
+                carry = (l1->val + l2->val + carry) / 10;
+                curr->next = new ListNode(sum);
+                curr = curr->next;
+                l1 = l1->next;
+                l2 = l2->next;
+            } else {
+                sum = (l1->val + carry) % 10;
+                carry = 0;
+                curr->next = new ListNode(sum);
+                curr = curr->next;
+                l1 = l1->next;
+            }         
+        }
+
+        while(l2 != NULL) {
+            sum = (l2->val + carry) % 10;
+            carry = 0;
+            curr->next = new ListNode(sum);
+            curr = curr->next;
+            l2 = l2->next;
+        }
+
+        return head;
     }
 };
