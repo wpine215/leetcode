4b037a1b0180d4f16485ac3c5740f71e4af969cf
wpine215 <wpine215@gmail.com>
2019-08-28 20:05:51 -0400

    Finished 3sum

--- array/3sum.py
+++ array/3sum.py
@@ -1,13 +1,36 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    def threeSum(self, nums):
+        nums.sort()
         triplets = []
-        lookback = set()
-        for i in nums:
-            for j in [x for x in nums if x != i]:
-                if -(i + j) in lookback:
-                    triplets.append([i, j, -(i+j)])
-                lookback.add(j)
-            lookback.clear()
+        for i in range(len(nums) - 2):
+            if nums[i] > 0: break
+            if i > 0 and nums[i] == nums[i-1]: continue
+
+            lhs = i + 1
+            rhs = len(nums) - 1
+            while lhs < rhs:
+                attempt = nums[i] + nums[lhs] + nums[rhs]
+
+                if attempt < 0:
+                    lhs += 1
+                elif attempt > 0:
+                    rhs -= 1
+                else:
+                    triplets.append([nums[i], nums[lhs], nums[rhs]])
+                    while lhs < rhs and nums[lhs] == nums[lhs + 1]:
+                        lhs += 1
+                    while lhs < rhs and nums[rhs] == nums[rhs - 1]:
+                        rhs -= 1
+                    lhs += 1
+                    rhs +=1
         return triplets
 
-# Current issue: solution set contains duplicate triplets when it should not
+def main():
+    s = Solution()
+    nums_ = [-2, 1, 3, -1, 6, 5, 4, -3]
+    res = s.threeSum(nums_)
+    print(res)
+
+if __name__ == "__main__":
+    main()
+
